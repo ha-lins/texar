6816fd7cfff8604525ec70714340b8b8d1627205
Shuai Lin <shuailin97@gmail.com>
2020-06-28 00:12:20 +0800

    Update config_train.py

--- examples/text_content_manipulation/config_train.py
+++ examples/text_content_manipulation/config_train.py
@@ -1,7 +1,7 @@
-from utils import get_scope_name_of_train_op
+from utils_e2e import get_scope_name_of_train_op
 
-max_epochs = int(8)
-steps_per_eval = int(600)
+max_epochs = int(0)  # int(0)
+steps_per_eval = int(985)
 
 infer_beam_width = 5
 infer_max_decoding_length = 50
@@ -20,21 +20,7 @@ train = {
                 'clip_norm': 15
             }
         },
-    },
-    'align': {
-        'optimizer': {
-            'type': 'AdamOptimizer',
-            'kwargs': {
-                'learning_rate': 1e-3
-            }
-        },
-        'gradient_clip': {
-            'type': 'clip_by_global_norm',
-            'kwargs': {
-                'clip_norm': 15
-            }
-        },
-    },
+    }
 }
 
 for name, hparams in train.items():
